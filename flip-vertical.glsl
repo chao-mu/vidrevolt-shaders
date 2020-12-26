15dd953df3afb3440cc4284797cf29d0b41d9cab
Danimal <danimal@bikegasm.org>
2020-12-26 14:36:08 -0500

    flip vertical

---
+++ flip-vertical.glsl
@@ -0,0 +1,16 @@
+#version 410
+
+layout (location = 0) out vec4 o;
+
+#pragma input vec3 img0
+#pragma input float mix
+
+#pragma include includes/util.inc.glsl
+
+void main() {
+    vec2 tc = img0_tc;
+    vec2 flipped = vec2(tc.x, img0_res.y - tc.y);
+
+    o.rgb = mix(input_img0(), input_img0(flipped), input_mix());
+    o.a = 1;
+}
